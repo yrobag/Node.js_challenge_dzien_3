3df5d9e2152b6dd7df17f35072a2d1d93cdc5867
jakub <gaboryjakub@gmail.com>
2018-01-31 18:55:01 +0100

    3rd day tasks

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,16 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-//Twój kod
+const crypto = require('crypto');
+
+
+const requestedPasswords = ['??TegoHasła','CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83',];
+const algorithms = ['sha256', 'sha512', 'md5', 'rmd160'];
+requestedPasswords.forEach(password =>{
+   algorithms.forEach(algorithm => {
+       let result = crypto.createHmac(algorithm, password).digest('hex');
+       if(result === MY_PWD_HASH){
+           console.log(`${password}(${algorithm})`)
+       }
+   });
+
+});
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,12 @@
-//Twój kod
+const crypto = require('crypto');
+const fs = require('fs');
+const pathToFile = process.argv[2];
+
+fs.readFile(pathToFile, 'utf-8', (err, data) => {
+   if(err !== null){
+       console.log(err);
+       return false;
+   }
+   let hash = crypto.createHmac('sha256', data).digest('hex');
+   console.log(hash);
+});
